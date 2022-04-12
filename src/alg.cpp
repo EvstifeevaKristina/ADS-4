9c84cc06b14f543f2864a3bab8ccb368ab5aec05
EvstifeevaKristina <94647555+EvstifeevaKristina@users.noreply.github.com>
2022-04-12 22:13:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,6 @@
 // Copyright 2021 NNTU-CS
-  void SortAlgo(int* data, int lenD) {
+  
+void SortAlgo(int* data, int lenD) {
   int j = 0;
   int tmp = 0;
   for (int i = 0; i < lenD; i++) {
@@ -14,6 +15,7 @@
     data[j] = tmp;
   }
 }
+
 int countPairs1(int* arr, int len, int value) {
   SortAlgo(arr, len);
   int n = 0;
@@ -27,6 +29,7 @@ int countPairs1(int* arr, int len, int value) {
   return n;
 }
  
+int countPairs2(int* arr, int len, int value) {
   SortAlgo(arr, len);
   int n = 0;
   for (int i = 0; i < len - 1; i++) {
@@ -38,6 +41,8 @@ int countPairs1(int* arr, int len, int value) {
   }
   return n;
 }
+
+int countPairs3(int *arr, int len, int value) {
     SortAlgo(arr, len);
 int n = 0;
 for (int i = 0; i < len - 1; i++) {
